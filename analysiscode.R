34f4cf29fc7327559fe55b50cf49d943221e9581
luiyusen97 <luiyusen97@gmail.com>
2020-07-21 18:41:37 +0800

    Added rawdata, documentation and faq files. analysiscode is for later copying into knitr.

--- analysiscode.R
+++ analysiscode.R
@@ -1,7 +1,22 @@
-url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
-if (!file.exists("rawdata.zip")){
-    download.file(url = url,
-                  destfile = "rawdata.zip")
+# loading packages
+library(tidyverse)
+
+# file downloading
+url <- c("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
+         "https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2Fpd01016005curr.pdf",
+         "https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf")
+if (!file.exists("rawdata.csv.bz2")){
+    download.file(url = url[1],
+                  destfile = "rawdata.csv.bz2")
 }
-unzip(zipfile = "rawdata.zip", 
-      exdir = "rawdata")
+if (!file.exists("rawdata_doc.pdf")){
+    download.file(url = url[2],
+                  destfile = "rawdata_doc.pdf")
+}
+if (!file.exists("rawdata_faq.pdf")){
+    download.file(url = url[3],
+                  destfile = "rawdata_faq.pdf")
+}
+
+#read data
+stormdata <- read.csv("rawdata.csv.bz2")
